a2bc9a21ee8488e3874dc8e7e3f5fa353c87439c
zb210 <69257420+zb210@users.noreply.github.com>
2020-08-05 18:37:55 +0200

    Add files via upload

---
+++ leap/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using Leap;
+
+
+namespace leap
+{
+    public partial class Form1 : Form, ILeapEventDelegate
+    {
+        private Controller controller;
+        private LeapEventListener listener;
+
+        public Form1()
+        {
+
+        InitializeComponent();
+            this.controller = new Controller();
+            this.listener = new LeapEventListener(this);
+            controller.AddListener(listener);
+        }
+
+       
+        delegate void LeapEventDelegate(string EventName);
+        public void LeapEventNotification(string EventName)
+        {
+            if (!this.InvokeRequired)
+            {
+                switch (EventName)
+                {
+                    case "onInit":
+                        MessageBox.Show("initialised");
+                        break;
+                    case "onConnect":
+                        MessageBox.Show("connected");
+                        break;
+                    case "onFrame":
+                        MessageBox.Show("onframe");
+                        break;
+                }
+            }
+            else
+            {
+                BeginInvoke(new LeapEventDelegate(LeapEventNotification), new object[] { EventName });
+            }
+        }
+    }
+    public interface ILeapEventDelegate
+    {
+        void LeapEventNotification(string EventName);
+    }
+
+    public class LeapEventListener : Listener
+    {
+        ILeapEventDelegate eventDelegate;
+
+        public LeapEventListener(ILeapEventDelegate delegateObject)
+        {
+            this.eventDelegate = delegateObject;
+        }
+        public override void OnInit(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onInit");
+        }
+        public override void OnConnect(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onConnect");
+        }
+        public override void OnFrame(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onFrame");
+        }
+        public override void OnExit(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onExit");
+        }
+        public override void OnDisconnect(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onDisconnect");
+        }
+    }
+}
+
+
